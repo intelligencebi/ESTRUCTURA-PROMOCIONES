09cfe3ac529677a320caba4f53e354a0117ecb0e
intelligencebi <intelligencebi282@gmail.com>
2025-11-03 12:54:35 -0300

    Update queries.py

--- app/utils/queries.py
+++ app/utils/queries.py
@@ -1,1 +1,13 @@
+from app.utils.supabase_client import supabase
 
+def get_resumen_promocion(promo_name):
+    res = supabase.rpc("resumen_por_promocion", {"promo_name": promo_name}).execute()
+    return res.data
+
+def get_seguimiento_promocion(promo_name, fecha_inicio=None, fecha_fin=None):
+    params = {"promo_name": promo_name}
+    if fecha_inicio and fecha_fin:
+        params["fecha_inicio"] = fecha_inicio
+        params["fecha_fin"] = fecha_fin
+    res = supabase.rpc("seguimiento_por_promocion", params).execute()
+    return res.data
