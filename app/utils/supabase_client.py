a0682f8129e1ae3b59620ded9bb60f49e7d88d5e
intelligencebi <intelligencebi282@gmail.com>
2025-11-03 12:54:20 -0300

    Update supabase_client.py

--- app/utils/supabase_client.py
+++ app/utils/supabase_client.py
@@ -1,1 +1,10 @@
+from supabase import create_client
+import streamlit as st
 
+@st.cache_resource
+def get_supabase():
+    url = st.secrets["supabase"]["url"]
+    key = st.secrets["supabase"]["key"]
+    return create_client(url, key)
+
+supabase = get_supabase()
